320ccea046feda6371b9f57fa89f81ef3fbc7f6e
贪婪的三文鱼 <1329009435@qq.com>
2023-11-09 20:58:18 +0800

    函数名修改

--- thinktank-admin/src/main/java/com/thinktank/admin/controller/ApplicationBlockController.java
+++ thinktank-admin/src/main/java/com/thinktank/admin/controller/ApplicationBlockController.java
@@ -3,11 +3,13 @@ package com.thinktank.admin.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.thinktank.admin.service.ApplicationBlockManageService;
 import com.thinktank.common.utils.R;
+import com.thinktank.common.validationgroups.QueryValidation;
 import com.thinktank.generator.dto.BlockApplicationBlockDto;
 import com.thinktank.generator.vo.BlockApplicationBlockVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -18,35 +20,35 @@ import org.springframework.web.bind.annotation.*;
  */
 @Api(tags = "申请创建板块管理接口")
 @RestController
-@RequestMapping("/applicationBlockManage")
-public class ApplicationBlockManageController {
+@RequestMapping("applicationBlockManage")
+public class ApplicationBlockController {
     @Autowired
     private ApplicationBlockManageService applicationBlockManageService;
 
-    @ApiOperation("板块创建申请分页查询")
+    @ApiOperation("板块创建申请记录分页查询")
     @PostMapping("page")
-    public R<IPage<BlockApplicationBlockVo>> page(@RequestBody BlockApplicationBlockDto blockApplicationBlockDto) {
+    public R<IPage<BlockApplicationBlockVo>> page(@RequestBody @Validated(QueryValidation.class) BlockApplicationBlockDto blockApplicationBlockDto) {
         IPage<BlockApplicationBlockVo> page = applicationBlockManageService.page(blockApplicationBlockDto);
         return R.success(page);
     }
 
     @ApiOperation("通过板块创建申请")
     @PutMapping("allow/{id}")
-    public R<String> allowApplicationBlock(@PathVariable("id") Long id) {
+    public R<String> allow(@PathVariable("id") Long id) {
         applicationBlockManageService.allow(id);
         return R.success("已通过该申请");
     }
 
     @ApiOperation("驳回板块创建申请")
     @PutMapping("reject/{id}")
-    public R<String> rejectApplicationBlock(@PathVariable("id") Long id) {
+    public R<String> reject(@PathVariable("id") Long id) {
         applicationBlockManageService.reject(id);
         return R.success("已驳回该申请");
     }
 
     @ApiOperation("删除板块创建申请记录")
     @DeleteMapping("{id}")
-    public R<String> deleteApplicationBlock(@PathVariable("id") Long id) {
+    public R<String> delete(@PathVariable("id") Long id) {
         applicationBlockManageService.delete(id);
         return R.success("已删除该申请记录");
     }
