4636a2cd71cae4c6477f8291bd078c4b7f7713a1
贪婪的三文鱼 <1329009435@qq.com>
2023-12-19 10:32:53 +0800

    新增创建板块信息同步es文档

--- thinktank-admin/src/main/java/com/thinktank/admin/service/impl/ManageBlockServiceImpl.java
+++ thinktank-admin/src/main/java/com/thinktank/admin/service/impl/ManageBlockServiceImpl.java
@@ -3,7 +3,10 @@ package com.thinktank.admin.service.impl;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.thinktank.admin.service.ManageBlockService;
+import com.thinktank.api.clients.SearchClient;
+import com.thinktank.api.doc.BlockInfoDoc;
 import com.thinktank.common.exception.ThinkTankException;
+import com.thinktank.common.utils.R;
 import com.thinktank.generator.dto.BlockInfoDto;
 import com.thinktank.generator.entity.BlockBigType;
 import com.thinktank.generator.entity.BlockInfo;
@@ -35,6 +38,9 @@ public class ManageBlockServiceImpl implements ManageBlockService {
     @Autowired
     private BlockSmallTypeMapper blockSmallTypeMapper;
 
+    @Autowired
+    private SearchClient searchClient;
+
     @Override
     public IPage<BlockInfoVo> page(BlockInfoDto blockInfoDto) {
         Page<BlockInfo> page = new Page<>();
@@ -61,13 +67,17 @@ public class ManageBlockServiceImpl implements ManageBlockService {
             throw new ThinkTankException("板块小分类不存在");
         }
 
-        if (blockInfoMapper.insert(blockInfo) > 0) {
-            return blockInfo;
+        if (blockInfoMapper.insert(blockInfo) == 0) {
+            log.error("新增板块失败，可能存在同名板块");
+            throw new ThinkTankException("新增板块失败，可能存在同名板块");
         }
 
-        log.error("新增板块失败，可能存在同名板块");
-        throw new ThinkTankException("新增板块失败，可能存在同名板块");
+        // 将板块信息写入elasticsearch数据库
+        R<BlockInfoDoc> result = searchClient.addBlockInfoDoc(blockInfo);
+
+        if (!result.getStatus().equals(200)) {
+            throw new ThinkTankException(result.getMsg());
+        }
+        return blockInfo;
     }
-
-
 }
